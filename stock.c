a3248c266b0cdc998130236d832368edef4a8ada
kzarnowski <45438132+kzarnowski@users.noreply.github.com>
2021-10-21 15:06:37 +0200

    Update stock.c

--- stock.c
+++ stock.c
@@ -6,11 +6,6 @@
 // COLORS
 #define RED "\e[31m"
 #define GRN "\e[32m"
-#define YEL "\e[33m"
-#define BLU "\e[34m"
-#define MAG "\e[35m"
-#define CYN "\e[36m"
-#define WHT "\e[37m"
 #define RESET "\e[0m"
 
 int main() {
@@ -22,16 +17,14 @@ int main() {
     
     char tab[60];
     char* names[] = { "ABC", "BMD", "CDX", "ASD", "WDI" }; // length(names) == N
-    char* format = "%s: %.2lf, ";
+    char* format = "%s: %s%.2lf\e[0m, ";
     
     int idx = 0;
     for (int i = 0; i < N; i++) {
-        idx += snprintf(tab + idx, 60, format, names[i], num[i]);
+        idx += snprintf(tab + idx, 60, format, names[i], num[i] > 500 ? GRN : RED, num[i]);
     }
     tab[idx-2] = '\n';
     tab[idx-1] = '\0';
     
     printf("%s", tab);
-    printf(RED "xxxx\n" RESET);
-    printf(YEL "yyy\n" RESET);
 }
