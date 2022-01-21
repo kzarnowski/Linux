f5d4d09591a260bcdec8c45d430c13d3c5cc1dbb
kzarnowski <zarnowski@student.agh.edu.pl>
2022-01-21 17:03:33 +0100

    lab8/2

--- lab8/zad2.c
+++ lab8/zad2.c
@@ -8,6 +8,9 @@
 
 void child();
 void parent();
+void handle();
+
+volatile struct timespec t1, t2;
 
 #define NANOSEC 1000000000L
 #define FL2NANOSEC(f)   {(long)(f), ((f) - (long)(f))*NANOSEC}
@@ -38,9 +41,9 @@ int main(int argc, char** argv) {
 
 void parent() {
     close(fd[0]);
-    signal(SIGPIPE, SIG_IGN);
+    signal(SIGPIPE, handle);
 
-    const struct timespec t = FL2NANOSEC(0.4);
+    const struct timespec t = FL2NANOSEC(0.6);
 
     char* message = "Hey\n";
 
@@ -50,6 +53,13 @@ void parent() {
         res = write(fd[1], message, 4);
         if (res == -1 && errno == EPIPE) {
             perror("Pipe is closed");
+            struct timespec t3, t4;
+            clock_gettime(CLOCK_REALTIME, &t3);
+            clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &t4);
+            printf("REAL_TIME    (handle): %ld.%ld\n", t1.tv_sec, t1.tv_nsec);
+            printf("REAL_TIME    (main):   %ld.%ld\n", t3.tv_sec, t3.tv_nsec);
+            printf("PROCESS_TIME (handle): %ld.%ld\n", t2.tv_sec, t2.tv_nsec);
+            printf("PROCESS_TIME (main):   %ld.%ld\n", t4.tv_sec, t4.tv_nsec);
             exit(EXIT_SUCCESS);
         }
     }
@@ -68,4 +78,12 @@ void child() {
             printf("%s", buffer);
         }
     }
+}
+
+void handle() {
+    struct timespec tt1, tt2;
+    clock_gettime(CLOCK_REALTIME, &tt1);
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &tt2);
+    t1 = tt1;
+    t2 = tt2;
 }
